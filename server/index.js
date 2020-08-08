f790fab8192070dc9db6c5d0897500654a209a81
Elliot Winkler <elliot.winkler@gmail.com>
2020-08-08 17:24:32 -0600

    Add grakn

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const fastify = require("fastify")({ logger: true });
+const GraknClient = require("grakn-client");
+
+async function openSession(keyspace) {
+  const client = new GraknClient("localhost:48555");
+  const session = await client.session(keyspace);
+  await session.close();
+  client.close();
+}
+
+openSession("social_network");
 
 fastify.get("/", async (request, reply) => {
+  openSession("social_network");
   return { hello: "world" };
 });
 
-async start() {
+async function start() {
   try {
     await fastify.listen(3000);
   } catch (err) {
